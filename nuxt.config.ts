6c010b8293525a2aa05b306e42cd9531f704396e
Tom5565 <37259081+Tom5565@users.noreply.github.com>
2023-03-08 04:15:30 +0900

    Add site metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,11 +16,16 @@ export default defineNuxtConfig({
                 href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap",
                 crossorigin: "",
               },
+              { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+              { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
             ],
-            title: "ICU B-Syllabus",
+            title: "Syllabus.icu",
             meta:[
-                {name:'description',content:'ICU B-syllabus'}
-            ]
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                {name:'description',content:'The Non-Official ICU Syllabus Search. Search the entire ICU syllabus using keywords!'},
+            ],
+            htmlAttrs:{lang:'en'}
         },
         pageTransition:{name:"page",mode: 'out-in'}
     },
