429a6dcc7eec8c02ae3bd81fefe1f4461824eddb
pentacoxian <c241494r@icu.ac.jp>
2023-03-26 02:16:16 +0000

    Fix page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
               { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
               { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
             ],
-            title: "Syllabus.icu",
+            title: "Syllabus.icu - ICUシラバス検索",
             meta:[
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
